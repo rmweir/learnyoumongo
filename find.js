0ba6c34183faa3c0acd2fb4a8253fa0248e30eb7
rmweir <ricardo.weir1@gmail.com>
2017-12-21 00:04:53 +0000

    i lied before, now it is good. don't forget how to comapre fields in find functions

--- find.js
+++ find.js
@@ -2,12 +2,13 @@ var mongo = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/learnyoumongo'; 
 
 mongo.connect(url, function(err, db) {
-	var findage = parseInt(process.argv[3]);
+	var findage = parseInt(process.argv[2]);
 	var collection = db.collection('parrots');
 	var array = collection.find({
-        	age: findage 
+        	age:{$gt: findage} 
         }).toArray(function(err, documents) {
-		console.log(docutments);	 
-    	});
-	db.close(); 	
+		console.log(documents);	 
+    		db.close();
+	});
+	// db.close(); 	
 });
